1dd7c1fbc556f467f5880df566dc1ef37949e5c8
aTimmYm <2korollev2@gmail.com>
2025-11-08 23:51:00 +0200

    startup update

--- startup.lua
+++ startup.lua
@@ -2,6 +2,33 @@ if bOS then error("System is already running!") end
 _G.bOS = {}
 bOS.start = bOS
 
+local sysConf = {
+    "Explorer sbin/explorer.lua sbin/ico/explorer.ico",
+    "Shell sbin/shell.lua sbin/ico/shell.ico",
+    "Settings sbin/settings.lua sbin/ico/settings.ico",
+    "Calculator sbin/calculator.lua sbin/ico/calculator.ico",
+    "MPlayer sbin/MPlayer.lua sbin/ico/player.ico",
+    "Polygon sbin/polygon.lua sbin/ico/default.ico",
+    "IMGBlittle utility/ImageBLITTLE.lua sbin/ico/default.ico",
+    "Paint rom/programs/fun/advanced/paint.lua sbin/ico/default.ico",
+    "converter sbin/shell.lua sbin/ico/default.ico",
+    "Messenger sbin/messenger.lua sbin/ico/messenger.ico"
+}
+
+if not fs.exists("usr") then fs.makeDir("usr") end
+if not fs.exists("home/Music") then fs.makeDir("home/Music") end
+if not fs.exists("usr/settings.conf") then
+    local file = fs.open("usr/settings.conf","w")
+    file.write("isMonitor=false\npalette=Default\nmonitorScale=1\n24format=true\nshow_seconds=false")
+    file.close()
+end
+if not fs.exists("usr/sys.conf") then
+    local file = fs.open("usr/sys.conf","w")
+    for _,v in pairs(sysConf) do
+    file.write(v.."\n")
+    end
+    file.close()
+end
 local c = require("lib.cfunc")
 local conf = c.readConf("usr/settings.conf")
 
